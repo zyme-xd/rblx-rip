eb597290c8f499f9b5d75ce24610a05c45e8bb3d
zyme <lolrichyisswag@gmail.com>
2020-12-19 22:21:33 -0600

    Changed source for template, no more issues.

--- index.js
+++ index.js
@@ -5,23 +5,22 @@ const readline = require('readline-promise').default.createInterface({
 });
 const fs = require('fs');
 
-main()
-async function main() {
+init()
+
+async function init() {
     let asset = parseInt(await readline.questionAsync('Enter an asset ID:  '))
     let regex = /\D/g
     if (isNaN(asset)) {
         console.log("You can't input a string")
-        return main()
+        init()
+        return
     }
     console.log(`Ok, getting the template for the asset id ${asset}`)
     let text = await fetch(`https://assetdelivery.roblox.com/v1/asset?id=${asset}`)
-        .then(res => res.text())
+                .then(res => res.text())
     let newId = text.split("<url>").join().split("</url>").join().split(",")[1].replace(regex, '')
-    await fetch(`https://thumbnails.roblox.com/v1/assets?assetIds=${newId}&size=700x700&format=Png&isCircular=false`)
-        .then(res => res.json())
-        .then(res => imageUrl = res.data[0].imageUrl)
-    let res = await fetch(imageUrl);
+    let res =  await fetch(`https://assetdelivery.roblox.com/v1/asset?id=${newId}`)
     let writestream = fs.createWriteStream('./folder/' + newId + '.png')
     res.body.pipe(writestream)
-    readline.close()
+     readline.close()
 }
