59308bf5c6ba082f1bcb4630c30084ac38917a1b
zyme <lolrichyisswag@gmail.com>
2020-12-21 19:59:54 -0600

    fixed empty catch, added dedicated download func

--- index.js
+++ index.js
@@ -18,9 +18,14 @@ _ __ | |__ | |_  __  ______   _ __ _ _ __
 console.log('Thanks for installing rblx-rip! Please keep in mind you cannot download off-sale assets.')
 main()
 async function main() {
-    try {
-        fs.mkdirSync('./assets/', {})
-    } catch (error) {}
+    if (!fs.existsSync("./assets/")) {
+        try {
+            fs.mkdirSync("./assets")
+        } catch(e) {
+            console.error("Error!");
+            throw e;
+        }
+    }
     let asset = parseInt(await readline.questionAsync('Enter an asset ID:  '))
     if (isNaN(asset)) {
         console.log("You can't input a string")
@@ -37,23 +42,27 @@ async function main() {
     }
     async function rip() {
         if (type == '.rbxm' || type == '.mp3' || type == '.webm' || type == ".rbxl") {
-            response = await fetch(`https://assetdelivery.roblox.com/v1/asset?id=${asset}`,{method:'GET',headers:{'User-Agent': 'Roblox/WinInet'}})
+            response = await download(asset, {method:'GET',headers:{'User-Agent': 'Roblox/WinInet'}});
             response.body.pipe(fs.createWriteStream(`./assets/${asset}${type}`))
         } else if (type == '.rbxmx') {
-            resp = await fetch(`https://assetdelivery.roblox.com/v1/asset?id=${asset}`).then(res => res.text())
+            resp = await download(asset).then(res => res.text())
             newId = resp.split(";")
             for (i = 0; newId.length > i; i++) {
-                res = await fetch(`https://assetdelivery.roblox.com/v1/asset?id=${newId[i]}`)
+                res = await download(newId[i])
                 response.body.pipe(fs.createWriteStream(`./assets/${newId[i]}${type}`))
             }
         }
          else {
-            response = await fetch(`https://assetdelivery.roblox.com/v1/asset?id=${asset}`).then(res => res.text())
+            response = await download(asset).then(res => res.text())
             newId = response.split("<url>").join().split("</url>").join().split(",")[1].replace(/\D/g, '')
-            res = await fetch(`https://assetdelivery.roblox.com/v1/asset?id=${newId}`)
+            res = await download(newId)
             response.body.pipe(fs.createWriteStream(`./assets/${newId}${type}`))
         }
         readline.close()
     }
+    
+}
 
+async function download(asset, headers) {
+    return await fetch(`https://assetdelivery.roblox.com/v1/asset?id=${asset}`,headers ? headers : {})
 }
